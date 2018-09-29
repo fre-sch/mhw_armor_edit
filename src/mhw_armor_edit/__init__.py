150b61c12b8ecc0bab6bab16d9c45a6db783acc9
Frederik Schumacher <frederik.schumacher@gmail.com>
2018-09-29 16:41:52 +0200

    load gmds into translation class, don't show unsupported file types in file tree

--- src/mhw_armor_edit/__init__.py
+++ src/mhw_armor_edit/__init__.py
@@ -4,6 +4,7 @@ import os
 import re
 import sys
 from collections import namedtuple
+from fnmatch import fnmatch
 
 from PyQt5.QtCore import Qt, QObject, pyqtSignal, pyqtSlot, QSize
 from PyQt5.QtGui import QKeySequence
@@ -13,60 +14,129 @@ from PyQt5.QtWidgets import (QApplication, QMainWindow, QSplitter,
                              QWidget, QMessageBox)
 
 from mhw_armor_edit.armor_editor import ArmorPieceWidget, ArmorEditor
-from mhw_armor_edit.crafting_editor import CraftingTableEditor
+from mhw_armor_edit.crafting_editor import (CraftingTableEditor,
+                                            CraftingRequirementsEditor)
 from mhw_armor_edit.ftypes.eq_crt import EqCrt
+from mhw_armor_edit.ftypes.gmd import Gmd
+from mhw_armor_edit.ftypes.itm import Itm
+from mhw_armor_edit.gmd_editor import GmdTableEditor
+from mhw_armor_edit.itm_editor import ItmTableEditor
 from mhw_armor_edit.shell_table_editor import ShellTableEditor
 from mhw_armor_edit.ftypes.am_dat import AmDat
 from mhw_armor_edit.ftypes.sh_tbl import ShellTable
+from mhw_armor_edit.utils import create_action
 from mhw_armor_edit.view_ctrl import ArmorPieceViewCtrl
 
-log = logging.getLogger(__name__)
+log = logging.getLogger()
 
 EditorPlugin = namedtuple("EditorPlugin", (
-    "regex",
+    "pattern",
     "data_factory",
     "widget_factory"
 ))
+
+Relations = {
+    r"common\equip\armor.am_dat": {
+        "crafting_model": r"common\equip\armor.eq_crt",
+    }
+}
 
 
 class FilePluginRegistry:
     registered = (
         EditorPlugin(
-            re.compile(r"\.am_dat$"),
+            "*.am_dat",
             AmDat,
             ArmorEditor,
         ),
         EditorPlugin(
-            re.compile(r"\.shl_tbl$"),
+            "*.shl_tbl",
             ShellTable,
             ShellTableEditor,
         ),
         EditorPlugin(
-            re.compile(r"\.eq_crt$"),
+            "*.eq_crt",
             EqCrt,
             CraftingTableEditor,
+        ),
+        EditorPlugin(
+            "*.gmd",
+            Gmd,
+            GmdTableEditor,
+        ),
+        EditorPlugin(
+            "*.itm",
+            Itm,
+            ItmTableEditor,
         )
     )
 
     @classmethod
     def get_plugin(cls, path):
         for plugin in cls.registered:
-            if plugin.regex.search(path):
+            if fnmatch(path, plugin.pattern):
                 return plugin
 
     @classmethod
-    def load_model(cls, path):
+    def load_model(cls, workspace, path, rel_path, is_relation=False):
         plugin = cls.get_plugin(path)
+        model = {}
         with open(path, "rb") as fp:
-            return plugin.data_factory.load(fp)
+            model["model"] = plugin.data_factory.load(fp)
+        if not is_relation:
+            model.update(cls._load_relations(rel_path, workspace))
+        return model
+
+    @classmethod
+    def _load_relations(cls, rpath, workspace):
+        rel_models = {}
+        relations = Relations.get(rpath)
+        if not relations:
+            return rel_models
+        for key, relation_rpath in relations.items():
+            relation_path, exists = workspace.get_path(relation_rpath)
+            if not exists:
+                rel_models[key] = None
+            else:
+                rel_model = cls.load_model(
+                    workspace, relation_path, relation_rpath, True)
+                rel_models[key] = rel_model["model"]
+        return rel_models
 
 
-def create_action(icon, title, handler, shortcut=None):
-    action = QAction(icon, title)
-    if shortcut is not None:
-        action.setShortcut(shortcut)
-    action.triggered.connect(handler)
-    return action
+class Translations:
+    Tables = {
+        "armor": r"common\text\steam\armor_eng.gmd",
+        "armor_series": r"common\text\steam\armor_series_eng.gmd",
+        "item": r"common\text\steam\item_eng.gmd",
+        "skill": r"common\text\vfont\skill_eng.gmd",
+        "skill_pt": r"common\text\vfont\skill_pt_eng.gmd",
+    }
+
+    def __init__(self):
+        self.tables = {}
+
+    def load(self, workspace):
+        for key, rpath in self.Tables.items():
+            self._load_table(workspace, key, rpath)
+
+    def _load_table(self, workspace, key, rpath):
+        abs_path, exists = workspace.get_path(rpath)
+        if not exists:
+            log.debug("translation file %s not found", abs_path)
+            return
+        with open(abs_path, "rb") as fp:
+            try:
+                self.tables[key] = Gmd.load(fp)
+                log.debug("translation file %s loaded", abs_path)
+            except Exception as e:
+                log.exception("failed loading translation file  %s", abs_path)
+
+    def get(self, table_name, index):
+        table = self.tables.get(table_name)
+        if not table:
+            return f"{table_name}{index}"
+        return table.get_string(index, f"{table_name}{index}")
 
 
 class Workspace(QObject):
@@ -81,10 +151,11 @@ class Workspace(QObject):
         self.root_path = None
         self.files = []
         self.file_models = {}
+        self.translations = Translations()
 
     def get_path(self, rel_path):
         path = os.path.join(self.root_path, rel_path)
-        exists = os.path.exists(rel_path)
+        exists = os.path.exists(path)
         return path, exists
 
     def get_rel_path(self, abs_path):
@@ -92,24 +163,25 @@ class Workspace(QObject):
 
     def set_root_path(self, path):
         self.root_path = path
+        self.translations.load(self)
         self.rootPathChanged.emit(self.root_path)
 
     def open_file(self, path):
         rel_path = self.get_rel_path(path)
         if path in self.files:
-            index = self.files.index(path)
             self.fileActivated.emit(path, rel_path)
         else:
-            self.files.append(path)
-            index = self.files.index(path)
             try:
-                self.file_models[path] = FilePluginRegistry.load_model(path)
+                model = FilePluginRegistry.load_model(self, path, rel_path)
+                model["translations"] = self.translations
+                self.file_models[path] = model
+                self.files.append(path)
                 self.fileOpened.emit(path, rel_path)
             except Exception as e:
+                log.exception("error loading path: %s", path)
                 self.fileLoadError.emit(path, rel_path, str(e))
 
     def close_file(self, path):
-        index = self.files.index(path)
         self.files.remove(path)
         self.file_models.pop(path)
         self.fileClosed.emit(path, self.get_rel_path(path))
@@ -152,7 +224,7 @@ class MainWindow(QMainWindow):
         self.init_actions()
         self.init_menu_bar()
         self.init_toolbar()
-        self.setWindowTitle("Editor")
+        self.setWindowTitle("MHW Content Suite")
         self.setGeometry(300, 300, 1000, 800)
         split = QSplitter(Qt.Horizontal, self)
         split.setChildrenCollapsible(False)
@@ -255,10 +327,14 @@ class MainWindow(QMainWindow):
         if path:
             self.file_system_model.setRootPath(path)
             self.file_tree_view.setRootIndex(self.file_system_model.index(path))
+            self.file_system_model.setNameFilterDisables(False)
+            self.file_system_model.setNameFilters(
+                plugin.pattern for plugin in FilePluginRegistry.registered
+            )
 
     def handle_open_workspace_action(self):
         path = QFileDialog.getExistingDirectory(parent=self)
-        self.workspace.set_root_path(path)
+        self.workspace.set_root_path(os.path.normpath(path))
 
     def handle_save_file_action(self):
         editor = self.editor_tabs.currentWidget()
@@ -275,6 +351,11 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
                         format="%(levelname)s %(message)s")
     app = QApplication(sys.argv)
+    app.setStyleSheet("""
+    QSplitter::handle:horizontal {
+        background: #DFDFDF;
+    }
+    """)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
