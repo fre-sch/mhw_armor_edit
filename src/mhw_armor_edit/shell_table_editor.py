1f91d14c72a66e5a65c429acb5deabe7a720ca6c
Frederik Schumacher <frederik.schumacher@gmail.com>
2018-09-29 16:36:29 +0200

    shell_table editor cleanup, uses sortfiltertableview

--- src/mhw_armor_edit/shell_table_editor.py
+++ src/mhw_armor_edit/shell_table_editor.py
@@ -2,7 +2,7 @@
 from PyQt5.QtWidgets import QWidget, QTableView, QStackedLayout
 
 from mhw_armor_edit.ftypes.sh_tbl import ShellTableEntry
-from mhw_armor_edit.tree import StructTableModel
+from mhw_armor_edit.struct_table import StructTableModel, SortFilterTableView
 
 """
 [ Normal v ]
@@ -18,18 +18,17 @@ class ShellTableEditor(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.model = None
-        self.table_view = QTableView(self)
+        self.table_model = StructTableModel(
+            ShellTableEntry.fields(), [])
+        self.table_view = SortFilterTableView(self)
+        self.table_view.setModel(self.table_model)
         self.setLayout(QStackedLayout(self))
         self.layout().addWidget(self.table_view)
 
     def set_model(self, model):
-        self.model = model
+        self.model = model["model"]
         if model is None:
-            self.table_view.setModel(None)
+            self.table_model.update([])
         else:
-            self.table_view.setModel(
-                StructTableModel(ShellTableEntry.fields(),
-                                 self.model.entries)
-            )
+            self.table_model.update(self.model.entries)
             self.table_view.resizeColumnsToContents()
-            self.table_view.resizeRowsToContents()
