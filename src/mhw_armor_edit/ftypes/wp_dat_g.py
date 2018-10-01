a2b5c62b695d707fb50f75f3fd00b1d276ed0f9e
Frederik Schumacher <frederik.schumacher@gmail.com>
2018-10-01 22:29:30 +0200

    wp_dat_g fields

--- src/mhw_armor_edit/ftypes/wp_dat_g.py
+++ src/mhw_armor_edit/ftypes/wp_dat_g.py
@@ -4,24 +4,63 @@ import struct
 from mhw_armor_edit.ftypes import Struct, InvalidDataError
 
 
-class WeaponDataGunEntry(metaclass=Struct):
-    STRUCT_SIZE = 60
+class WpDatGEntry(metaclass=Struct):
+    STRUCT_SIZE = 68
     STRUCT_FIELDS = (
-        ("index", "<H"),
-        ("unknown1", "<17b", True),
+        ("index", "<I"),
+        ("byte1", "<B"),
+        ("byte2", "<B"),
+        ("short3", "<h"),
+        ("short4", "<h"),
+        ("short5", "<h"),
+        ("byte6", "<B"),
+        ("byte7", "<B"),
+        ("byte8", "<B"),
+        ("byte9", "<B"),
+        ("byte10", "<B"),
+        ("byte11", "<B"),
+        ("byte12", "<B"),
         ("cost", "<I"),
-        ("rarity", "<b"),
-        ("unknown2", "<12b", True),
+        ("rarity", "<B"),
+        ("true_damage", "<H"),
+        ("unknown20", "<B"),
+        ("unknown21", "<B"),
+        ("affinity", "<b"),
+        ("unknown23", "<B"),
+        ("unknown24", "<B"),
+        ("unknown25", "<B"),
+        ("unknown26", "<B"),
+        ("unknown27", "<B"),
+        ("unknown28", "<B"),
+        ("unknown29", "<B"),
         ("shell_table_id", "<H"),
-        ("unknown3", "<b"),
+        ("unknown30", "<B"),
         ("num_gem_slots", "<B"),
         ("gem_slot1_lvl", "<B"),
         ("gem_slot2_lvl", "<B"),
         ("gem_slot3_lvl", "<B"),
-        ("unknown4", "<13b", True),
+        ("unknown31", "<B"),
+        ("unknown32", "<B"),
+        ("unknown33", "<B"),
+        ("unknown34", "<B"),
+        ("unknown35", "<B"),
+        ("unknown36", "<B"),
+        ("unknown37", "<B"),
+        ("unknown38", "<B"),
+        ("unknown39", "<B"),
+        ("unknown40", "<B"),
+        ("unknown41", "<B"),
+        ("unknown42", "<B"),
+        ("unknown43", "<B"),
         ("special_ammo_type", "<B"),
-        ("unknown5", "<B"),
+        ("unknown44", "<B"),
+        ("unknown45", "<B"),
+        ("unknown46", "<B"),
+        ("gmd_name_index", "<H"),
+        ("gmd_description_index", "<H"),
         ("skill_id", "<H"),
+        ("unknown51", "<B"),
+        ("unknown52", "<B"),
     )
 
     def __init__(self, data, offset):
@@ -29,7 +68,7 @@ class WeaponDataGunEntry(metaclass=Struct):
         self.offset = offset
 
 
-class WeaponDataGun:
+class WpDatG:
     MAGIC = 0x01B1
     NUM_ENTRY_OFFSET = 2
     ENTRY_OFFSET = 6
@@ -45,8 +84,8 @@ class WeaponDataGun:
 
     def _load_entries(self):
         for i in range(0, self.num_entries):
-            offset = self.ENTRY_OFFSET + i * WeaponDataGunEntry.STRUCT_SIZE
-            yield WeaponDataGunEntry(self.data, offset)
+            offset = self.ENTRY_OFFSET + i * WpDatGEntry.STRUCT_SIZE
+            yield WpDatGEntry(self.data, offset)
 
     def __getitem__(self, item):
         return self.entries[item]
@@ -67,18 +106,16 @@ class WeaponDataGun:
         for item in self.find(**attrs):
             return item
 
-    def save(self, fp):
-        fp.write(self.data)
-
     @classmethod
     def check_header(cls, data):
         result = struct.unpack_from("<H", data, 0)
         if result[0] != cls.MAGIC:
             raise InvalidDataError(
-                f"magic byte invalid: expected {cls.MAGIC:04X}, found {result[0]:04X}")
+                f"magic byte invalid: expected {cls.MAGIC:04X}, "
+                f"found {result[0]:04X}")
         result = struct.unpack_from("<I", data, cls.NUM_ENTRY_OFFSET)
         num_entries = result[0]
-        entries_size = num_entries * WeaponDataGunEntry.STRUCT_SIZE
+        entries_size = num_entries * WpDatGEntry.STRUCT_SIZE
         data_entries_size = len(data) - cls.ENTRY_OFFSET
         if data_entries_size != entries_size:
             raise InvalidDataError(f"total size invalid: "
@@ -91,3 +128,6 @@ class WeaponDataGun:
         data = bytearray(fp.read())
         cls.check_header(data)
         return cls(data)
+
+    def save(self, fp):
+        fp.write(self.data)
