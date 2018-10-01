df677264fa519f64eb58a77411bc9a39497b0f81
Frederik Schumacher <frederik.schumacher@gmail.com>
2018-10-01 22:32:47 +0200

    remove useless comment, add fake index

--- src/mhw_armor_edit/shell_table_editor.py
+++ src/mhw_armor_edit/shell_table_editor.py
@@ -4,22 +4,13 @@ from PyQt5.QtWidgets import QWidget, QTableView, QStackedLayout
 from mhw_armor_edit.ftypes.sh_tbl import ShellTableEntry
 from mhw_armor_edit.struct_table import StructTableModel, SortFilterTableView
 
-"""
-[ Normal v ]
-[ Pierce   ]
-[ Spread   ]
-     count   type   reload
-1    [____]  [____] [____]
-2    [____]  [____] [____]
-3    [____]  [____] [____]
 
-"""
 class ShellTableEditor(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.model = None
         self.table_model = StructTableModel(
-            ShellTableEntry.fields(), [])
+            ("index", ) + ShellTableEntry.fields(), [])
         self.table_view = SortFilterTableView(self)
         self.table_view.setModel(self.table_model)
         self.setLayout(QStackedLayout(self))
