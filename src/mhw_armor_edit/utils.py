766e9c4db852a17a114b16e05e4c5861a5cc3c17
Frederik Schumacher <frederik.schumacher@gmail.com>
2019-01-28 20:51:33 +0100

    remove unused code

--- src/mhw_armor_edit/utils.py
+++ src/mhw_armor_edit/utils.py
@@ -2,9 +2,7 @@
 import logging
 
 from PyQt5.QtCore import QModelIndex, Qt, QAbstractItemModel
-from PyQt5.QtWidgets import (QAction, QGroupBox, QFormLayout, QLabel, QWidget,
-                             QItemDelegate, QComboBox)
-
+from PyQt5.QtWidgets import (QAction, QWidget, QItemDelegate, QComboBox)
 
 log = logging.getLogger(__name__)
 
@@ -44,25 +42,6 @@ def create_action(icon, title, handler, shortcut=None, checkable=None):
         action.setCheckable(checkable)
     action.triggered.connect(handler)
     return action
-
-
-class FormGroupbox(QGroupBox):
-    def __init__(self, title, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.setStyleSheet("QGroupBox {font-weight:bold}")
-        self.setLayout(QFormLayout(self))
-        if title:
-            self.setTitle(title)
-            self.setFlat(True)
-
-    def __iadd__(self, other):
-        left, right = other
-        if not isinstance(left, QWidget):
-            left = QLabel(str(left))
-        if not isinstance(right, QWidget):
-            right = QLabel(str(right))
-        self.layout().addRow(left, right)
-        return self
 
 
 class ItemDelegate(QItemDelegate):
