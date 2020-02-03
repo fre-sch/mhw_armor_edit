bff19824f73da4176f7654404bdffa3bfedcb8b2
Frederik Schumacher <frederik.schumacher@gmail.com>
2020-02-03 20:22:08 +0100

    basic skl_dat support #16

--- src/mhw_armor_edit/ftypes/skl_dat.py
+++ src/mhw_armor_edit/ftypes/skl_dat.py
@@ -4,13 +4,17 @@ from mhw_armor_edit.ftypes import StructFile, Struct
 
 
 class SklDatEntry(Struct):
-    STRUCT_SIZE = 11
+    STRUCT_SIZE = 19
     skill_id: ft.ushort()
     level: ft.ubyte()
     param1: ft.ushort()
     param2: ft.ushort()
     param3: ft.ushort()
     param4: ft.ushort()
+    param5: ft.ushort()
+    param6: ft.ushort()
+    param7: ft.ushort()
+    param8: ft.ushort()
 
 
 class SklDat(StructFile):
